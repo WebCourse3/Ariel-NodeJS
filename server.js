71ea442b39135b4b6d5772771131dd318e280af2
lychtear <lychtear@gmail.com>
2017-10-31 15:12:38 +0200

    chat

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', socket => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', msg => {
+        if (msg.includes('/setColor')) {
+            socket.broadcast.emit('set color', msg);
+        } else {
+            socket.broadcast.emit('chat message', msg);
+        }
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
